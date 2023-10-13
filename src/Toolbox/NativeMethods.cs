50ccff05a96f544d58c40e39935a329cea4ed263
LaughingLeader <laughingleader@gmail.com>
2023-10-12 23:07:14 -0500

    Swap from IntPtr to nint type (C# 11)

--- src/Toolbox/NativeMethods.cs
+++ src/Toolbox/NativeMethods.cs
@@ -5,14 +5,14 @@ namespace Toolbox
 	public static partial class NativeMethods
 	{
 		[LibraryImport("kernel32.dll", EntryPoint = "LoadLibraryA")]
-		public static partial IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFileName);
+		public static partial nint LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFileName);
 
 		[LibraryImport("kernel32.dll", SetLastError = true)]
-		public static partial IntPtr GetProcAddress(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)] string procedureName);
+		public static partial nint GetProcAddress(nint hModule, [MarshalAs(UnmanagedType.LPStr)] string procedureName);
 
 		[LibraryImport("kernel32.dll")]
 		[return: MarshalAs(UnmanagedType.Bool)]
-		public static partial bool FreeLibrary(IntPtr libraryReference);
+		public static partial bool FreeLibrary(nint libraryReference);
 
 		public static void ThrowExceptionForLastWin32Error()
 		{
