5d1039e5578eeaaad55224095796a541a08c8219
LaughingLeader <laughingleader@gmail.com>
2024-09-06 11:18:54 -0500

    Add a "clear" command to textboxes

---
+++ src/GUI/AppCommands.cs
@@ -0,0 +1,40 @@
+﻿using ReactiveUI;
+
+using System.Reactive;
+using System.Windows.Controls;
+
+namespace DivinityModManager
+{
+	public static class AppCommands
+	{
+		public static ReactiveCommand<object, Unit> Clear { get; }
+
+		private static void ClearText(object sender)
+		{
+			if(sender is MenuItem menuItem)
+			{
+				var cm = menuItem.FindVisualParent<ContextMenu>();
+				if(cm != null && cm.PlacementTarget is TextBox tb)
+				{
+					tb.Clear();
+				}
+			}
+			else if(sender is TextBox textBox)
+			{
+				textBox.Clear();
+			}
+			else if(sender is ContextMenu cm)
+			{
+				if (cm.PlacementTarget is TextBox tb)
+				{
+					tb.Clear();
+				}
+			}
+		}
+
+		static AppCommands()
+		{
+			Clear = ReactiveCommand.Create<object>(ClearText);
+		}
+	}
+}
